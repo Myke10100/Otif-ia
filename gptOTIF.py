8c26007e9d7d7c00d051df5223d11aeefc8bc71e
Myke10100 <m.munevar@ofiservices.com>
2024-07-15 15:53:01 -0500

    Update gptOTIF.py

--- gptOTIF.py
+++ gptOTIF.py
@@ -1,5 +1,7 @@
 import streamlit as st
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key= st.secrets["OPENAI_API_KEY"])
 import json
 import requests
 
@@ -15,8 +17,7 @@ with col1:
 with col2:
     st.title("Chat de asistencia Ofi Services")
 
-# Acceder a la clave API de OpenAI desde secretos
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+# Acceder a la clave API de OpenAI directamente
 
 # Cargar la configuración del modelo
 if "openai_model" not in st.session_state:
@@ -99,16 +100,13 @@ if prompt := st.chat_input("Hágame una pregunta sobre gestión de pedidos"):
     with st.chat_message("assistant"):
         messages = [{"role": m["role"], "content": m["content"]} for m in st.session_state.messages]
         try:
-            response = openai.ChatCompletion.create(
-                model=st.session_state["openai_model"],
-                messages=messages
-            )
-            response_text = response.choices[0].message["content"]
+            response = client.chat.completions.create(model=st.session_state["openai_model"],
+            messages=messages)
+            response_text = response.choices[0].message.content
         except Exception as e:
             response_text = f"Error al obtener la respuesta de OpenAI: {str(e)}"
 
         # Mostrar la respuesta del asistente
         st.markdown(response_text)
     st.session_state.messages.append({"role": "assistant", "content": response_text})
-
 
