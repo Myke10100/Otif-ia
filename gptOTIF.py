d942517fad777b0db71e100c4a57cef198c81094
Myke10100 <m.munevar@ofiservices.com>
2024-07-24 09:20:31 -0500

    Update gptOTIF.py

--- gptOTIF.py
+++ gptOTIF.py
@@ -58,7 +58,7 @@ project_info_text = json.dumps(project_info, indent=2)
 initial_prompt = (
     "You will be a virtual assistant who will act as a specialized consultant, with high knowledge in analysis related to OTIF processes."
     "You are a virtual assistant, who will answer questions like CHAT GPT, in the answers you should not see formulas, nor formulas, nor where you take the data, only short and specific answers, the calculations will be done with the following fields, but remember this should not be shown as a result."
-    "Columns with data up to June remember, take the unique values, do not duplicate any information:\n"
+    "Columns with data up to April remember, take unique values, do not duplicate any information the date format is DD-MM-YYYYY please take into account the number of orders per date since most of the analysis is done by quantity.:\n"
     "PO\n"
     "Creation Date\n"
     "Order Value\n"
